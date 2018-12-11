d09d435f516a761df161734fc71d567ca321b417
Ho Phuong Nam <hophuongnam@gmail.com>
2018-12-11 14:13:27 +0700

    Tue Dec 11 14:13:27 +07 2018

--- OneDrive - my.smccd.edu/watch.sh
+++ OneDrive - my.smccd.edu/watch.sh
@@ -2,39 +2,33 @@
 
 while true
 do
+    cd "/Data/bunkei.ziten/OneDrive - my.smccd.edu"
     inotifywait -e move_self bunkei*docx
     sleep 5
-    pushd .
+    # pushd .
 
     ls bunkei*docx | ./docx2xml.sh
     sleep 5
 
-    cd ..
+    cd /Data/bunkei.ziten
     ruby bunkei.ziten.rb
-
 
     # rm *docx.xml
     sleep 5
 
-
     git add --all
     git commit -m "$(date)"
     git push -u origin master
-
 
     sleep 5
 
-
     # cp ../time4vps.html/* ../hophuongnam.github.io/
 
-
     # rsync -av --delete --exclude=.* ../time4vps.html/ ../hophuongnam.github.io/
-    cd ../hophuongnam.github.io
+    cd /Data/hophuongnam.github.io
     git add --all
     git commit -m "$(date)"
     git push -u origin master
-    cd ../bunkei.ziten
 
-
-    popd
+    # popd
 done
